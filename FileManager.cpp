460350f35524b4367f7e3603aef1f3380cadf30e
Sebastian Muñoz Mora <smm9308@gmail.com>
2021-07-24 20:03:27 -0500

    implement wav writer

--- FileManager.cpp
+++ FileManager.cpp
@@ -2,12 +2,9 @@
 #include <fstream>
 #include <byteswap.h>
 #include <string.h>
+#include <vector>
 
 using namespace std;
-
-void writeByte(ofstream &file, int value, int size) {
-    file.write(reinterpret_cast<const char *> (&value), size);
-}
 
 class FileManager {
 
@@ -17,14 +14,21 @@ class FileManager {
     int subChunk1Size;
     int subChunk2Size;
 
-    void writeAudioData(ofstream file, float data) {
-
+    void writeByte(ofstream &file, int value, int size) {
+        file.write(reinterpret_cast<const char *> (&value), size);
     }
 
-    ofstream createWavHeaderFile(string fileName,
+    void writeAudioData(ofstream &file, vector<float> data) {
+        for(auto sample: data){
+            writeByte(file, __bswap_16(sample), 2);
+        }
+    }
+
+    ofstream createWavHeaderFile(string &fileName,
                                 int samplingFrequency,
                                 int bitsPerSample,
-                                int numChannels) {
+                                int numChannels,
+                                vector<float> data) {
         ofstream file;
         file.open(fileName+".wav", ios::binary);
         if(file.is_open()) {
@@ -33,16 +37,25 @@ class FileManager {
             file << "WAVE";
 
             file << "fmt ";
-            writeByte(file, __bswap_32(16), 4);
+            writeByte(file, 16, 4);
             writeByte(file, __bswap_16(1), 2);
-            writeByte(file, __bswap_16(2), 2);
-            writeByte(file, __bswap_32(44100), 4);
+            writeByte(file, __bswap_16(numChannels), 2);
+            writeByte(file, __bswap_32(samplingFrequency), 4);
             writeByte(file, __bswap_32(byteRate), 4);
-            writeByte(file, __bswap_32(blockAlign), 4);
-            writeByte(file, __bswap_32(bitsPerSample), 4);
+            writeByte(file, __bswap_32(blockAlign), 2);
+            writeByte(file, __bswap_32(bitsPerSample), 2);
             file << "data";
             file << "----";
-
+            writeAudioData(file, data);
+            int end = file.tellp();
+            file.seekp(40);
+            subChunk2Size = end - 44;
+            cout << subChunk2Size << endl;
+            writeByte(file, __bswap_32(subChunk2Size), 4);
+            file.seekp(4);
+            chunkSize = 36 + subChunk2Size;
+            cout << chunkSize << endl;
+            writeByte(file, __bswap_32(chunkSize), 4);
             file.close();
         }
 
@@ -52,13 +65,18 @@ class FileManager {
 
     public:
     FileManager(string fileName,int samplingFrequency, int numChannels,
-        float data, int bitsPerSample ) {
+        vector<float> data, int bitsPerSample ) {
             byteRate = samplingFrequency * numChannels * bitsPerSample / 8;
             blockAlign = numChannels * bitsPerSample / 8;
             chunkSize = 0;
             subChunk1Size = __bswap_32(16);
             subChunk2Size = samplingFrequency * 2 * bitsPerSample / 8;
 
-        createWavHeaderFile(fileName, samplingFrequency, bitsPerSample, numChannels);
+        createWavHeaderFile(fileName, samplingFrequency, bitsPerSample, numChannels, data);
     }
 };
+
+/*int main () {
+    vector<float> data = {0.1,0.2,0.3,0.1};
+    FileManager fm("hello", 44100, 2, data, 16);
+}*/
--- SignalGenerator.cpp
+++ SignalGenerator.cpp
@@ -5,14 +5,20 @@
 #include <cmath>
 #include <vector>
 #include "Sine.cpp"
+#include "FileManager.cpp"
 
 using namespace std;
 
 int main () {
-	SineWave mySine(2, 1000);
+	SineWave mySine(1, 1000);
 	vector <float> sineWave = mySine.getWave();
+	const int currentSampleRate = mySine.getSamplingRate();
+	const int currentDuration = mySine.getDuration();
+	cout << sineWave[10] << endl;
+	FileManager fs("hh", currentSampleRate, 1, sineWave, 16);
 
-	cout << "mySine: " << sineWave[100] << endl;
+	cout << "mySine: " << currentSampleRate << endl;
+	cout << "mySine: " << currentDuration << endl;
 
 	return 0;
 }
